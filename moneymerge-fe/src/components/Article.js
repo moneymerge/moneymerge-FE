cbc475703b20c75d673d7335cd6678ef82be7397
leeyejin <leeyejin113@gmail.com>
2024-06-02 22:55:08 +0900

    크기 조정, 글꼴, 단건조회

--- moneymerge-fe/src/components/Article.js
+++ moneymerge-fe/src/components/Article.js
@@ -2,49 +2,50 @@ import Link from "next/link";
 
 const Article = () => {
   return (
-    <div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="406"
-        height="385"
-        fill="none"
-      >
-        <path fill="#000" d="M3.78 5.978H406V385H3.78z" />
-        <path fill="#FFAFBC" stroke="#000" d="M.5.5h401.22v378.022H.5z" />
-        <path
-          fill="#F1FF9D"
-          stroke="#000"
-          d="M1.256 31.587H401.72v345.739H1.256z"
-        />
-        <path
-          fill="#FFE9E9"
-          stroke="#000"
-          d="M9.694 40.941h382.833v324.423H9.694z"
-        />
-        <path fill="#fff" d="M337.5 6.5h24v19h-24z" />
-        <path fill="#fff" d="M337.5 6.5h24v19h-24z" />
-        <path fill="#fff" d="M337.5 6.5h24v19h-24z" />
-        <path stroke="#000" d="M337.5 6.5h24v19h-24z" />
-        <path fill="#050505" d="m349 8.5 7.361 8.556h-14.722L349 8.5Z" />
-        <path fill="#050505" d="M345.357 14.204h7.771V22.5h-7.771z" />
-        <path fill="#fff" d="M367.5 6.5h24v19h-24z" />
-        <path fill="#fff" d="M367.5 6.5h24v19h-24z" />
-        <path fill="#fff" d="M367.5 6.5h24v19h-24z" />
-        <path stroke="#000" d="M367.5 6.5h24v19h-24z" />
-        <path
-          stroke="#000"
-          strokeWidth="3"
-          d="M0-1.5h17.41"
-          transform="matrix(.66516 .7467 -.78453 .62009 373 10)"
-        />
-        <path
-          stroke="#000"
-          strokeWidth="3"
-          d="M0-1.5h17.41"
-          transform="matrix(.66516 -.7467 .78453 .62009 374.419 23)"
-        />
-      </svg>
-    </div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="652"
+      height="514"
+      fill="none"
+    >
+      <path fill="#000" d="M6.071 7.981H652V514H6.071z" />
+      <path fill="#FFAFBC" stroke="#000" d="M.5.5h644.929v505.019H.5z" />
+      <path
+        fill="#F1FF9D"
+        stroke="#000"
+        d="M1.714 42.003h643.715v461.919H1.714z"
+      />
+      <path
+        fill="#FFE9E9"
+        stroke="#000"
+        d="M15.264 54.491h615.401v433.46H15.264z"
+      />
+      <path fill="#fff" d="M589.869 8.51h39.148v25.701h-39.148z" />
+      <path fill="#fff" d="M589.869 8.51h39.148v25.701h-39.148z" />
+      <path fill="#fff" d="M589.869 8.51h39.148v25.701h-39.148z" />
+      <path stroke="#000" d="M589.869 8.51h39.148v25.701h-39.148z" />
+      <path fill="#fff" d="M541.692 8.51h39.148v25.701h-39.148z" />
+      <path fill="#fff" d="M541.692 8.51h39.148v25.701h-39.148z" />
+      <path fill="#fff" d="M541.692 8.51h39.148v25.701h-39.148z" />
+      <path stroke="#000" d="M541.692 8.51h39.148v25.701h-39.148z" />
+      <path
+        fill="#050505"
+        d="m560.463 11.348 11.821 11.422h-23.642l11.821-11.422Z"
+      />
+      <path fill="#050505" d="M554.613 18.963h12.48v11.076h-12.48z" />
+      <path
+        stroke="#000"
+        stroke-width="3"
+        d="M0-1.5h25.438"
+        transform="matrix(.73109 .68228 -.83573 .54914 599.005 13.35)"
+      />
+      <path
+        stroke="#000"
+        stroke-width="3"
+        d="M0-1.5h25.438"
+        transform="matrix(.73109 -.68228 .83573 .54914 601.284 30.706)"
+      />
+    </svg>
   );
 };
 
