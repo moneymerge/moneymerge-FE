207e47e1e94131bb443f2517e49f991c467c4e78
leeyejin <leeyejin113@gmail.com>
2024-06-16 22:24:48 +0900

    쿠키 설정

--- moneymerge-fe/src/app/auth/naver/callback/page.js
+++ moneymerge-fe/src/app/auth/naver/callback/page.js
@@ -1,24 +1,24 @@
-"use client"
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 const KakaoCallback = () => {
   const router = useRouter();
 
   useEffect(() => {
-    const code = new URL(window.location.href).searchParams.get('code');
+    const code = new URL(window.location.href).searchParams.get("code");
     fetch(`http://3.39.225.191/auth/naver/callback?code=${code}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((result) => result.json())
+      .then((result) => {
+        saveTokensToCookie(result.data.accessToken, result.data.refreshToken);
+        router.push("/");
       })
-        .then((result) => result.json())
-        .then((result) => {
-            saveTokensToCookie(result.data.accessToken, result.data.refreshToken);
-            router.push('/');
-        })
-        .catch((error) => console.error("Error fetching token data:", error));
+      .catch((error) => console.error("Error fetching token data:", error));
   }, []);
 
   return <div>Loading...</div>;
@@ -27,23 +27,27 @@ const KakaoCallback = () => {
 export default KakaoCallback;
 
 function saveTokensToCookie(accessToken, refreshToken) {
-    // Access Token 쿠키 설정
-    const accessTokenExpiresInSec = 2 * 60 * 60; // 2시간
-    setCookie('AccessToken', accessToken, accessTokenExpiresInSec);
-  
-    // Refresh Token 쿠키 설정
-    const refreshTokenExpiresInSec = 14 * 24 * 60 * 60; // 2주 (14일)
-    setCookie('RefreshToken', refreshToken, refreshTokenExpiresInSec);
-  
-    console.log("토큰을 쿠키에 저장했습니다.");
-  }
-  
-  function setCookie(name, value, maxAgeInSeconds) {
-    const expires = new Date();
-    expires.setTime(expires.getTime() + maxAgeInSeconds * 1000);
-    const cookieOptions = {
-        path: '/',
-        expires: expires.toUTCString()
-    };
-    document.cookie = `${name}=${value}; ${Object.entries(cookieOptions).map(entry => entry.join('=')).join('; ')}`;
-  }
+  // Access Token 쿠키 설정
+  const accessTokenExpiresInSec = 2 * 60 * 60; // 2시간
+  setCookie("AccessToken", accessToken, accessTokenExpiresInSec);
+
+  // Refresh Token 쿠키 설정
+  const refreshTokenExpiresInSec = 14 * 24 * 60 * 60; // 2주 (14일)
+  setCookie("RefreshToken", refreshToken, refreshTokenExpiresInSec);
+
+  console.log("토큰을 쿠키에 저장했습니다.");
+}
+
+function setCookie(name, value, maxAgeInSeconds) {
+  const expires = new Date();
+  expires.setTime(expires.getTime() + maxAgeInSeconds * 1000);
+  const cookieOptions = {
+    path: "/",
+    secure: false, // HTTPS에서만 쿠키 전송
+    sameSite: "None", // Cross-Site 접근 허용
+    expires: expires.toUTCString(),
+  };
+  document.cookie = `${name}=${value}; ${Object.entries(cookieOptions)
+    .map((entry) => entry.join("="))
+    .join("; ")}`;
+}
